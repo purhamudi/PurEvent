d34b66e69ee71360096db4784d2d1dc898408343
Fadi <fadi.muhammad@personetics.com>
2025-02-28 20:14:46 +0200

    fixes

--- src/ComingSoon.tsx
+++ src/ComingSoon.tsx
@@ -1,44 +1,41 @@
-import logo from './assets/logos/logo.png';
-import Text from './components/Library/Text/Text.component';
+import logo from "./assets/logos/logo.png";
+// import Text from './components/Library/Text/Text.component';
 
 import { motion } from "framer-motion"; // Import Framer Motion
 
 function ComingSoon() {
-    return (
-      <div className="h-screen flex flex-col justify-center items-center bg-brand-50 text-center px-6">
-        {/* Logo with Animation */}
-        <motion.div
-          initial={{ opacity: 0, y: -22 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-          className="w-full flex justify-center my-6"
-        >
-          <img src={logo} alt="Logo" className="w-56 md:w-96" />
-        </motion.div>
-  
-        {/* Animated Text */}
-        <motion.h1
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 0.7 }}
-          className="text-2xl md:text-3xl font-semibold text-gray-700 my-4"
-        >
-          Wir sind noch in der Aufwärmphase.
-        </motion.h1>
-  
-        <motion.h1
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1.2, delay: 1.7 }}
-          className="text-3xl md:text-4xl font-bold text-gray-800 my-2"
-        >
-          Coming soon! ;-)
-        </motion.h1>
-      </div>
-    );
-  }
-  
-export default ComingSoon
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-brand-50 text-center px-6">
+      {/* Logo with Animation */}
+      <motion.div
+        initial={{ opacity: 0, y: -22 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="w-full flex justify-center my-6"
+      >
+        <img src={logo} alt="Logo" className="w-56 md:w-96" />
+      </motion.div>
 
+      {/* Animated Text */}
+      <motion.h1
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.7 }}
+        className="text-2xl md:text-3xl font-semibold text-gray-700 my-4"
+      >
+        Wir sind noch in der Aufwärmphase.
+      </motion.h1>
 
+      <motion.h1
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2, delay: 1.7 }}
+        className="text-3xl md:text-4xl font-bold text-gray-800 my-2"
+      >
+        Coming soon! ;-)
+      </motion.h1>
+    </div>
+  );
+}
 
+export default ComingSoon;
