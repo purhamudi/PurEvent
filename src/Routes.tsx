71384942d1e7cfbb7d1c5436514d6efc76fcfb98
Fadi <fadi.muhammad@personetics.com>
2025-02-28 20:18:57 +0200

    removed main route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,37 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
-import NotFound from "./Pages/NotFound";
-import { Layout } from "./Pages/Layout";
-import AboutUs from "./Pages/AboutUs";
-import HomePage from "./Pages/HomePage";
+// import NotFound from "./Pages/NotFound";
+// import { Layout } from "./Pages/Layout";
+// import AboutUs from "./Pages/AboutUs";
+// import HomePage from "./Pages/HomePage";
 import ComingSoon from "./ComingSoon";
 
-export default function pCreateBrowserRouter(){
-    return createBrowserRouter([
+export default function pCreateBrowserRouter() {
+  return createBrowserRouter(
+    [
       {
         path: "",
-        element:<ComingSoon />
+        element: <ComingSoon />,
       },
-      {
-        path: "v1",
-        element: <Layout/>,
-        children:[
-          {
-            path: "",
-            element: <HomePage />,
-          },
-          {
-            path: "uber-uns",
-            element: <AboutUs />,
-          },
-          {
-            path: "*",
-            element: <NotFound />,
-          },
-        ]
-      },
-    ], {
-      // basename: (window.location.pathname === "/robots.txt") ? undefined : "/", 
-    });
-  }
-  
-
+      // {
+      //   path: "v1",
+      //   element: <Layout/>,
+      //   children:[
+      //     {
+      //       path: "",
+      //       element: <HomePage />,
+      //     },
+      //     {
+      //       path: "uber-uns",
+      //       element: <AboutUs />,
+      //     },
+      //     {
+      //       path: "*",
+      //       element: <NotFound />,
+      //     },
+      //   ]
+      // },
+    ],
+    {
+      // basename: (window.location.pathname === "/robots.txt") ? undefined : "/",
+    }
+  );
+}
