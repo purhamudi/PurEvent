cc9e05fd16484bf844c9a802f6443c3172ec3991
Fadi <fadi.muhammad@personetics.com>
2025-03-07 00:41:54 +0200

    fix path

--- src/sections/Homepage/ScrollSequence/ScrollImageSequence.tsx
+++ src/sections/Homepage/ScrollSequence/ScrollImageSequence.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 
+// import img from '../../../assets/images/background/dump/videos/test/'
 const frameCount = 160; // Adjust based on how many images you have
 const images = Array.from(
   { length: frameCount },
   (_, i) =>
-    `/src/assets/images/background/dump/videos/test/vid_${String(
+    `../../../assets/images/background/dump/videos/test/vid_${String(
       i + 1
     ).padStart(4, "0")}.png`
 );
